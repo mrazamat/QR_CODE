60be80b01b01b5d236c9995781d2f393c7ce0184
mrazamat <azamat00189@mail.ru>
2018-10-20 11:33:29 +0500

    QR Code Reader

---
+++ src/main/java/QRCodeReader.java
@@ -0,0 +1,40 @@
+import com.google.zxing.*;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+
+public class QRCodeReader {
+    private static String decodeQRCode(File qrCodeimage) throws IOException {
+        BufferedImage bufferedImage = ImageIO.read(qrCodeimage);
+        LuminanceSource source = new BufferedImageLuminanceSource(bufferedImage);
+        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+
+        try {
+            Result result = new MultiFormatReader().decode(bitmap);
+            return result.getText();
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            File file = new File("/home/coder/Desktop/facebook_qrcode.png");
+            String decodedText = decodeQRCode(file);
+            if (decodedText==null){
+                System.out.println("No QR Code found in image");
+            } else {
+                System.out.println("Decoded text = " + decodedText);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
